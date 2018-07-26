682f868dee2e090b79a1ffc0695b0720a7c3f3ba
hoatle <hoatle@teracy.com>
2018-07-26 21:53:24 +0700

    clean up

--- teracy-dev-ext.rb
+++ teracy-dev-ext.rb
@@ -1,8 +1,3 @@
-# -*- mode: ruby -*-
-# vi: set ft=ruby :
-
-teracy_lib_dir = File.expand_path('../../../lib', __dir__)
-$LOAD_PATH.unshift(teracy_lib_dir) unless $LOAD_PATH.include?(teracy_lib_dir)
 
 k8s_setup_lib_dir = File.expand_path('./lib', __dir__)
 $LOAD_PATH.unshift(k8s_setup_lib_dir) unless $LOAD_PATH.include?(k8s_setup_lib_dir)
