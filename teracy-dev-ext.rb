1e31f1745a4d0c625254423fd24ba4da5b5a3e37
hoatle <hoatle@teracy.com>
2018-07-29 16:25:28 +0700

    add TODO, minor code refactoring

--- teracy-dev-ext.rb
+++ teracy-dev-ext.rb
@@ -1,7 +1,5 @@
-
-k8s_setup_lib_dir = File.expand_path('./lib', __dir__)
-$LOAD_PATH.unshift(k8s_setup_lib_dir) unless $LOAD_PATH.include?(k8s_setup_lib_dir)
-
+lib_dir = File.expand_path('./lib', __dir__)
+$LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
 require 'teracy-dev'
 require 'teracy-dev-k8s'
